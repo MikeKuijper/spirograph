e572692fbcf0c0d0d766ff218cb8158f1e92d5de
Mike Kuijper <36420387+MikeKuijper@users.noreply.github.com>
2018-12-19 21:30:21 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,7 +7,10 @@ let canvas;
 let started = false;
 let n = 3;
 
+
 function start() {
+  let p5Canvas = document.getElementById('defaultCanvas0');
+  p5Canvas.style.visibility = "";
   wipers.splice(0);
   history.splice(0);
   for (index in form) {
@@ -18,12 +21,20 @@ function start() {
     });
   }
   print(wipers);
+  window.scrollTo(0,document.body.scrollHeight);
 
   started = true;
 }
 
+function reset() {
+  wipers.splice(0);
+  history.splice(0);
+  let p5Canvas = document.getElementById('defaultCanvas0');
+  p5Canvas.style.visibility = "hidden";
+}
+
 function setup() {
-  canvas = createCanvas(900, 800);
+  canvas = createCanvas(1000, 800);
   canvas.position((windowWidth - width) / 2);
   canvas.parent('sketch-holder');
   k = height / 5;
@@ -33,9 +44,10 @@ function setup() {
 
 function draw() {
   canvas.position((windowWidth - width) / 2);
-  updateTextInput();
+  //updateTextInput();
+  //updateSlider();
 
-  if (started) {
+  if (started && wipers.length > 0) {
     background(255);
     translate(width / 2, height / 2);
     angleMode(DEGREES);
@@ -82,6 +94,9 @@ function draw() {
       wipers[j].angle += wipers[j].speed * 0.45;
     }
   }
+  // else if (started) {
+  //   background(255, 255, 255, 0);
+  // }
 }
 
 let history = [];
@@ -99,70 +114,81 @@ function trace() {
   }
 }
 
-// function createForm(n) {
-//   for (let a = 0; a < n; a++) {
-//     var element1 = document.createElement("input");
-//     element1.setAttribute("class", "numberInput");
-//     element1.setAttribute("type", "number");
-//     element1.setAttribute("id", `formEntry-Length-${a}`);
-//     element1.setAttribute("value", "4000");
-//     var elementA = document.getElementById("settings");
-//     elementA.appendChild(element1);
-//
-//     var element2 = document.createElement("input");
-//     element2.setAttribute("class", "numberInput");
-//     element2.setAttribute("type", "number");
-//     element2.setAttribute("id", `formEntry-Speed-${a}`);
-//     element2.setAttribute("value", "4000");
-//     var elementB = document.getElementById("settings");
-//     elementB.appendChild(element2);
-//
-//     var element3 = document.createElement("input");
-//     element3.setAttribute("class", "numberInput");
-//     element3.setAttribute("type", "number");
-//     element3.setAttribute("id", `formEntry-Angle-${a}`);
-//     element3.setAttribute("value", "4000");
-//     var elementC = document.getElementById("settings");
-//     elementC.appendChild(element3);
-//     let endline = document.createElement("br");
-//     elementC.appendChild(endline);
-//   }
-// }
-
 let form = [];
 
 function makeForm(n) {
   for (let a = 0; a < n; a++) {
-    createP("Segment " + (a + 1)).parent("settings");
+    let column = `column-${a}`;
+    var elementA = document.getElementById(column);
+
+    var label = document.createElement("h1");
+    label.style.color = "#FFFFFF";
+    label.innerText = "Segment " + (a + 1);
+    var newline = document.createElement("br");
+    elementA.appendChild(label);
+
+    var label = document.createElement("label");
+    label.style.color = "#FFFFFF";
+    label.innerText = "Length";
+    var newline = document.createElement("br");
+    elementA.appendChild(label);
+    elementA.appendChild(newline);
+
     let lengthSlider = createSlider(0, 300, 150);
-    lengthSlider.parent("settings");
+    lengthSlider.elt.oninput = updateTextInput;
+    lengthSlider.parent(column);
 
     var lengthText = document.createElement("input");
     lengthText.setAttribute("class", "numberInput");
     lengthText.setAttribute("type", "number");
     lengthText.setAttribute("id", `lengthText-${a}`);
-    lengthText.setAttribute("value", lengthSlider.value()/100);
-    var elementA = document.getElementById("settings");
+    lengthText.setAttribute("value", lengthSlider.value() / 100);
+    lengthText.setAttribute("min", 0);
+    lengthText.setAttribute("max", 3);
+    lengthText.setAttribute("onchange", "updateSlider()");
     elementA.appendChild(lengthText);
 
+    var label = document.createElement("label");
+    label.style.color = "#FFFFFF";
+    label.innerText = "Speed";
+    var newline = document.createElement("br");
+    elementA.appendChild(label);
+    elementA.appendChild(newline);
+
     let speedSlider = createSlider(-500, 500, 10);
-    speedSlider.parent("settings");
+    speedSlider.elt.oninput = updateTextInput;
+    speedSlider.parent(column);
 
     var speedText = document.createElement("input");
     speedText.setAttribute("class", "numberInput");
     speedText.setAttribute("type", "number");
     speedText.setAttribute("id", `speedText-${a}`);
-    speedText.setAttribute("value", speedSlider.value()/10);
+    speedText.setAttribute("value", speedSlider.value() / 10);
+    speedText.setAttribute("min", -50);
+    speedText.setAttribute("max", 50);
+    speedText.setAttribute("onchange", "updateSlider()");
     elementA.appendChild(speedText);
 
+
+    var label = document.createElement("label");
+    label.style.color = "#FFFFFF";
+    label.innerText = "Starting angle";
+    var newline = document.createElement("br");
+    elementA.appendChild(label);
+    elementA.appendChild(newline);
+
     let angleSlider = createSlider(-180, 180, 0);
-    angleSlider.parent("settings");
+    angleSlider.elt.oninput = updateTextInput;
+    angleSlider.parent(column);
 
     var angleText = document.createElement("input");
     angleText.setAttribute("class", "numberInput");
     angleText.setAttribute("type", "number");
     angleText.setAttribute("id", `angleText-${a}`);
     angleText.setAttribute("value", angleSlider.value());
+    angleText.setAttribute("min", -180);
+    angleText.setAttribute("max", 180);
+    angleText.setAttribute("onchange", "updateSlider()");
     elementA.appendChild(angleText);
 
     form.push({
@@ -177,9 +203,19 @@ function makeForm(n) {
 }
 
 function updateTextInput() {
+
   for (x in form) {
-    form[x].lengthText.setAttribute("value", form[x].length.value()/100);
-    form[x].speedText.setAttribute("value", form[x].speed.value()/10);
-    form[x].angleText.setAttribute("value", form[x].angle.value());
+    form[x].lengthText.value = form[x].length.value() / 100;
+    form[x].speedText.value = form[x].speed.value() / 10;
+    form[x].angleText.value = form[x].angle.value();
+  }
+}
+
+function updateSlider() {
+
+  for (x in form) {
+    form[x].length.elt.value = form[x].lengthText.value * 100;
+    form[x].speed.elt.value = form[x].speedText.value * 10;
+    form[x].angle.elt.value = form[x].angleText.value;
   }
 }
